4ea6f903150583f5440b43c1d0d2bd21f304df3f
Juanma Gómez <juanma1999@gmail.com>
2025-02-25 23:34:34 +0100

    Tiro realizado a falta del bucle

--- Tiro.py
+++ Tiro.py
@@ -1,4 +1,17 @@
 import math
-angulo_rad=math.radians(angulo)
-alcance=(velocidad**2)*math.sin(2*angulo_rad)/gravedad
-altura_max = (velocidad**2)*(math.sin(angulo_rad**2)/(2*gravedad))
+angulo = 0
+velocidad = 0
+gravedad = 9.8
+
+def calculo_tiro():
+    velocidad = float(input("Introduzca la velocidad inicial: "))
+    angulo = float(input("Introduzca el angulo: "))
+    gravedad = float(input("Introduzca el valor de la gravedad: "))
+    angulo_rad=math.radians(angulo)
+    alcance=(velocidad**2)*math.sin(2*angulo_rad)/gravedad
+    distancia = round(alcance,2)
+    altura_max = (velocidad**2)*(math.sin(angulo_rad**2)/(2*gravedad))
+    altura = round(altura_max,2)
+    print(f"La distancia recorrida es de {distancia} con una altura máxima de {altura}")
+
+calculo_tiro()
