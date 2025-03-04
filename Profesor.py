3f17d314886f98f3e13755d5b2903453fc81d1c9
Juanma Gomez <juanma1999@gmail.com>
2025-03-04 13:40:29 +0100

    Ejercicio Nuevacuenta

--- Profesor.py
+++ Profesor.py
@@ -9,8 +9,8 @@ Va a tener:
 
 class Profesor:
     
-    def __init__(self):
-        self.nombre = " "
+    def __init__(self,nombre):
+        self.nombre = nombre
         self.asignatura = " "
         self.salario = float()
 
@@ -23,9 +23,21 @@ class Profesor:
     def get_asignatura(self):
         return self.asignatura
     
-def calcular_sueldo():
-    
-    num_horas = random.randint(18,40)
-    valor_horas = 67
+    def set_salario(self,salario):
+        self.salario = salario
+        
+    def get_salario(self):
+        return self.salario
+
+    def calcular_sueldo():
+        num_horas = int(input("Introduzca las horas de clases que ha impartido este mes: "))
+        valor_horas = 50
+        salario = num_horas * valor_horas
+        print(f"su sueldo este mes es de: {salario}€")
+
+profesor1 = Profesor()
+profesor1.nombre = "Pepe"
+Profesor.calcular_sueldo()
+
     
 
--- nuevacuenta.py
+++ nuevacuenta.py
@@ -1,18 +1,35 @@
 import random
 
-def almacenar_datos():
-    cuenta = []
-    banco = []
-  
-    for i in range(0,5):
-        
-        clientes = input("Introduzca su nombre y apellidos: ")
-        numeros = str(random.randint(1,9999999999))
-        cuenta.append(clientes)
-        cuenta.append(numeros)
-        banco.extend(cuenta)
-        #print(cuenta)
-    print(banco)
+class Cuenta:
 
+    cuentas_creadas = []
 
-almacenar_datos()
+    def __init__(self,saldo_inicial):
+        while True:
+            self.numero = random.randint(1000000000,9999999999)
+            if self.numero not in Cuenta.cuentas_creadas:
+                self.saldo = saldo_inicial
+                self.riesgo = 1
+                Cuenta.cuentas_creadas.append(self.numero)
+                break
+
+    def setnumero(self, num):
+        self.numero = num
+
+    def getnumero(self):
+        return self.numero
+    
+    def getsaldo(self):
+        return self.saldo
+    
+    def ingreso(self,cantidad):
+        self.saldo += cantidad
+
+    def retirada(self,cantidad):
+        self.saldo -= cantidad
+
+cuenta1 = Cuenta(1000)
+cuenta2 = Cuenta(100)
+cuenta3 = Cuenta(540)
+
+print(Cuenta.cuentas_creadas)
